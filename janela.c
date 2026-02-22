d45c0990d4dc62ea56de63f96817f9dc5708339e
sirius1618 <sagittarius_a1.618@proton.me>
2026-02-22 11:11:52 -0300

    style: Tira os comentarios desnecessarios

--- janela.c
+++ janela.c
@@ -4,7 +4,9 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-// estrutura representa a conexão com o servidor 
+#define COR_BRANCA_FUNDO 0Xffffff
+#define COR_VERDE_FUNDO 0X79d221
+
 Display* conetcar_servidor_grafico() {
     Display *d = XOpenDisplay(NULL); 
 
@@ -22,28 +24,26 @@ void fechar_conexao(Display *d) {
     }
 }
 
-
-// Dou o comando de criar janela -> envia ao servidor de gerenciar janelas -> servidor renderiza a janela (e coletas os eventos do input e manda ao cliente).
 void criar_janela(Display *d) {
     Window janela_pai = XDefaultRootWindow(d);
 
     int x = 0;
     int y = 0;
+
     unsigned int largura = 800;
     unsigned int altura = 600;
+
     int largura_borda = 5; 
     int profundidade_cor = CopyFromParent;
     unsigned int classe_janela = InputOutput;
-    // define como os pixels são interpretados (modelo de cor, marcara RGB, Organização dos bits)
-    Visual* visual_janela = CopyFromParent;
-    // indica quais campos da estrutura `atributes ` serão usados onde separamos usando OR
-    int valor_atributo = CWBackingPixel;
-    // contem os valores dos atributos escolhidos acima. 
-    XSetWindowAttributes atributos_janela = {};
-    atributos_janela.background_pixel = 0xffafe9af; // cor de fundo. 
 
-    Window janela = XCreateWindow(d, 
-        janela_pai, 
+    Visual* visual_janela = CopyFromParent;
+    int valor_atributo = CWBackingPixel;
+    XSetWindowAttributes atributos_janela = {};
+    atributos_janela.background_pixel = COR_BRANCA_FUNDO; 
+
+    Window janela = XCreateWindow(
+        d, janela_pai, 
         x, y,
         largura, altura,
         largura_borda,
